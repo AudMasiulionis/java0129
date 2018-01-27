cea7468aa918f8859220299eb97c06b0638c9b12
audrius.masiulionis <audrius.masiulionis@gmail.com>
2018-01-27 21:55:47 +0200

    Initial

---
+++ src/lesson1/completed/Console.java
@@ -0,0 +1,33 @@
+package lesson1.completed;
+
+public class Console {
+
+    public static void main(String[] args) {
+        //1
+        System.out.println("Hello World!");
+
+        //2
+        System.out.println("Hello Java!");
+        System.out.println("Hello Coding!");
+        System.out.println("Hello VCS!");
+
+        //3
+        System.out.println(12334);
+
+        //4
+        System.out.print(1);
+        System.out.print(2);
+        System.out.print(3);
+
+        //5
+        System.out.println(2 + 2);
+
+        //This is a comment
+
+        /*
+        This is
+        a multiline
+        comment
+        */
+    }
+}
---
+++ src/lesson1/completed/Methods.java
@@ -0,0 +1,9 @@
+package lesson1.completed;
+
+public class Methods {
+    public static void main(String[] args) {
+
+
+
+    }
+}
